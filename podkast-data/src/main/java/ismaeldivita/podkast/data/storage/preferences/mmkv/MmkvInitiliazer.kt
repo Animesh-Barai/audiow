8fd65bc6af653bdee6547a2c5355ec11fca4e0f5
ismaeldivita <ismaeldivita@gmail.com>
2018-12-22 15:08:44 -0200

    Move android version setup to project instead each module

--- podkast-data/src/main/java/ismaeldivita/podkast/data/storage/preferences/mmkv/MmkvInitiliazer.kt
+++ podkast-data/src/main/java/ismaeldivita/podkast/data/storage/preferences/mmkv/MmkvInitiliazer.kt
@@ -1,13 +1,13 @@
-//package ismaeldivita.podkast.data.storage.preferences.mmkv
-//
-//import android.app.Application
-//import com.tencent.mmkv.MMKV
-//import ismaeldivita.podkast.common.android.ApplicationInitializer
-//
-//class MmkvInitiliazer : ApplicationInitializer {
-//
-//    override fun initialize(application: Application) {
-//        MMKV.initialize(application)
-//    }
-//
-//}
+package ismaeldivita.podkast.data.storage.preferences.mmkv
+
+import android.app.Application
+import com.tencent.mmkv.MMKV
+import ismaeldivita.podkast.common.android.ApplicationInitializer
+
+class MmkvInitiliazer : ApplicationInitializer {
+
+    override fun initialize(application: Application) {
+        MMKV.initialize(application)
+    }
+
+}
