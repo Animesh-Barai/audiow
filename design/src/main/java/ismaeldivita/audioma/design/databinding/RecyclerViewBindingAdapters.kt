6df9004aa64c0d2d89e61e1f94f31898c9197d89
ismaeldivita <ismaeldivita@gmail.com>
2019-09-28 22:33:58 +0100

    Add space divider databinding adapters and update genre feed style

--- design/src/main/java/ismaeldivita/audioma/design/databinding/RecyclerViewBindingAdapters.kt
+++ design/src/main/java/ismaeldivita/audioma/design/databinding/RecyclerViewBindingAdapters.kt
@@ -1,7 +1,13 @@
 package ismaeldivita.audioma.design.databinding
 
+import android.graphics.Rect
+import android.graphics.drawable.Drawable
+import android.graphics.drawable.InsetDrawable
 import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
+import android.view.View
+
 
 interface BindableAdapter<T> {
     fun setData(data: T)
@@ -17,4 +23,50 @@ fun <T> setData(recyclerView: RecyclerView, data: T?) {
     if (adapter is BindableAdapter<*>) {
         (adapter as BindableAdapter<T>).setData(data)
     }
+}
+
+@BindingAdapter("verticalSpaceDecoration")
+fun verticalSpaceDecoration(recyclerView: RecyclerView, space: Float) {
+    recyclerView.addItemDecoration(
+        object : RecyclerView.ItemDecoration() {
+            override fun getItemOffsets(
+                outRect: Rect,
+                view: View,
+                parent: RecyclerView,
+                state: RecyclerView.State
+            ) {
+                val position = parent.getChildAdapterPosition(view)
+                val spaceInt = space.toInt()
+
+                val rect: Rect = when (position) {
+                    0 -> Rect(0, spaceInt, 0, spaceInt)
+                    else -> Rect(0, 0, 0, spaceInt)
+                }
+                outRect.set(rect)
+            }
+        }
+    )
+}
+
+@BindingAdapter("horizontalSpaceDecoration")
+fun horizontalSpaceDecoration(recyclerView: RecyclerView, space: Float) {
+    recyclerView.addItemDecoration(
+        object : RecyclerView.ItemDecoration() {
+            override fun getItemOffsets(
+                outRect: Rect,
+                view: View,
+                parent: RecyclerView,
+                state: RecyclerView.State
+            ) {
+                val position = parent.getChildAdapterPosition(view)
+                val spaceInt = space.toInt()
+
+                val rect: Rect = when (position) {
+                    0 -> Rect(spaceInt, 0, spaceInt, 0)
+                    else -> Rect(0, 0, spaceInt, 0)
+                }
+                outRect.set(rect)
+            }
+        }
+    )
 }
