32fa49de1509a4705cfe773b9d86b942889368f9
ismaeldivita <ismaeldivita@gmail.com>
2019-11-02 00:11:09 +0000

    Add basic header to Podcast detail

--- podcast/podcast_feature_detail/src/main/java/ismaeldivita/audioma/podcast/feature/detail/ui/detail/PodcastDetailFragment.kt
+++ podcast/podcast_feature_detail/src/main/java/ismaeldivita/audioma/podcast/feature/detail/ui/detail/PodcastDetailFragment.kt
@@ -1,7 +1,41 @@
 package ismaeldivita.audioma.podcast.feature.detail.ui.detail
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.bumptech.glide.Glide
 import ismaeldivita.audioma.core.android.ui.ViewModelFragment
+import ismaeldivita.audioma.podcast.feature.detail.R
+import ismaeldivita.audioma.podcast.feature.detail.databinding.PodcastFeatureDetailFragmentBinding
 
 class PodcastDetailFragment : ViewModelFragment<PodcastDetailViewModel>() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val binding = DataBindingUtil.inflate<PodcastFeatureDetailFragmentBinding>(
+            inflater,
+            R.layout.podcast_feature_detail_fragment,
+            container,
+            false
+        )
+
+        with(binding) {
+            lifecycleOwner = this@PodcastDetailFragment
+            imageLoader = Glide.with(this@PodcastDetailFragment)
+            vm = viewModel
+        }
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.init()
+    }
 
 }
--- podcast/podcast_feature_detail/src/main/java/ismaeldivita/audioma/podcast/feature/detail/ui/detail/PodcastDetailViewModel.kt
+++ podcast/podcast_feature_detail/src/main/java/ismaeldivita/audioma/podcast/feature/detail/ui/detail/PodcastDetailViewModel.kt
@@ -1,8 +1,27 @@
 package ismaeldivita.audioma.podcast.feature.detail.ui.detail
 
+import androidx.lifecycle.MutableLiveData
+import io.reactivex.rxkotlin.subscribeBy
 import ismaeldivita.audioma.core.android.viewmodel.BaseViewModel
+import ismaeldivita.audioma.core.data.repository.Repository
+import ismaeldivita.audioma.core.monitoring.log.Logger
+import ismaeldivita.audioma.core.util.reactive.SchedulersProvider
+import ismaeldivita.audioma.podcast.data.model.Podcast
 import javax.inject.Inject
 
-class PodcastDetailViewModel @Inject constructor() : BaseViewModel() {
+class PodcastDetailViewModel @Inject constructor(
+    private val podcastRepository: Repository<Podcast>,
+    private val schedulersProvider: SchedulersProvider
+) : BaseViewModel() {
+
+    val podcast = MutableLiveData<Podcast>()
+
+    fun init() {
+        podcastRepository.getAll()
+            .subscribeOn(schedulersProvider.io())
+            .doOnSuccess { Logger.d(it.toString()) }
+            .subscribeBy { podcast.postValue(it.random()) }
+            .registerDisposable()
+    }
 
 }
