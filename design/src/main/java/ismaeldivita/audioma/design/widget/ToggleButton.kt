78b7927fad9881ba15b8d72c9845cded1a476d95
ismaeldivita <ismaeldivita@gmail.com>
2019-11-03 16:20:51 +0000

    Add ToggleButton confirmation dialog API and listener

--- design/src/main/java/ismaeldivita/audioma/design/widget/ToggleButton.kt
+++ design/src/main/java/ismaeldivita/audioma/design/widget/ToggleButton.kt
@@ -10,6 +10,19 @@ import com.google.android.material.button.MaterialButton
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import ismaeldivita.audioma.design.R
 
+/**
+ *  A toggle button based on [MaterialButton] which apply two different styles based on
+ *  [android.R.attr.state_selected].
+ *
+ *  A confirmation dialog will popup on deselected events if
+ *  [R.styleable.ToggleButton_confirmationMessage] and
+ *  [R.styleable.ToggleButton_confirmationActionLabel] are setup.
+ *
+ *  We're using as workaround [android.R.attr.state_selected] state since [MaterialButton] is not
+ *  updating the drawable state when the [android.R.attr.state_checked]
+ *
+ *  @see [onToggleListener]
+ */
 class ToggleButton @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null
@@ -17,6 +30,8 @@ class ToggleButton @JvmOverloads constructor(
 
     private val checkedLabel: String
     private val uncheckedLabel: String
+
+    val onToggleListener: ((@ParameterName("isActive") Boolean) -> Unit)? = null
 
     init {
         val a = context.obtainStyledAttributes(attrs, R.styleable.ToggleButton)
@@ -31,6 +46,12 @@ class ToggleButton @JvmOverloads constructor(
             addState(intArrayOf(android.R.attr.state_selected), checkedDrawable)
             addState(intArrayOf(), uncheckedDrawable)
         }
+        val confirmationTitle = a.getString(R.styleable.ToggleButton_confirmationTitle)
+            ?: resources.getString(R.string.confirmation)
+
+        val confirmationMessage = a.getString(R.styleable.ToggleButton_confirmationMessage)
+        val confirmationActionLabel = a.getString(R.styleable.ToggleButton_confirmationActionLabel)
+
         checkedLabel = a.getStringOrThrow(R.styleable.ToggleButton_checkedLabel)
         uncheckedLabel = a.getStringOrThrow(R.styleable.ToggleButton_uncheckedLabel)
 
@@ -39,14 +60,18 @@ class ToggleButton @JvmOverloads constructor(
         isSelected = false
 
         setOnClickListener {
-            if (!isSelected) {
+            /**
+             * If confirmationMessage and confirmationActionLabel is setup shows the confirmation
+             * dialog on unselected events
+             */
+            if (!isSelected || (confirmationMessage == null && confirmationActionLabel == null)) {
                 isSelected = !isSelected
             } else {
                 MaterialAlertDialogBuilder(context)
-                    .setMessage("Abc asldkaj dasdlkasjd aslk")
-                    .setTitle("Title foo")
-                    .setPositiveButton("Unsubscribe") { _, _ -> isSelected = false }
-                    .setNegativeButton("Cancel") { dialog, _ -> dialog.cancel() }
+                    .setTitle(confirmationTitle)
+                    .setMessage(confirmationMessage)
+                    .setPositiveButton(confirmationActionLabel) { _, _ -> isSelected = false }
+                    .setNegativeButton(android.R.string.cancel) { dialog, _ -> dialog.cancel() }
                     .show()
             }
         }
@@ -54,8 +79,15 @@ class ToggleButton @JvmOverloads constructor(
         a.recycle()
     }
 
+
     override fun setSelected(selected: Boolean) {
+        val wasSelected = isSelected
         super.setSelected(selected)
+
+        if (wasSelected == selected) return
+
+        onToggleListener?.invoke(selected)
+
         val label = if (selected) checkedLabel else uncheckedLabel
 
         if (text != label) {
