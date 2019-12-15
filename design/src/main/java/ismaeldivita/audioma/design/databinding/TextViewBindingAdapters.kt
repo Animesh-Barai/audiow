c0ebf29c86eb17624df9aa2bd8bd912bf0281b69
ismaeldivita <ismaeldivita@gmail.com>
2019-12-15 02:24:30 +0000

    Load episode descripton

--- design/src/main/java/ismaeldivita/audioma/design/databinding/TextViewBindingAdapters.kt
+++ design/src/main/java/ismaeldivita/audioma/design/databinding/TextViewBindingAdapters.kt
@@ -4,27 +4,19 @@ import android.widget.TextView
 import androidx.core.text.HtmlCompat
 import androidx.databinding.BindingAdapter
 
-@BindingAdapter(
-    "htmlText",
-    "htmlTextFormatBreakLine",
-    requireAll = false
-)
+@BindingAdapter("htmlText")
 fun htmlText(
     view: TextView,
-    text: String?,
-    formatBreakLine: Boolean
+    text: String?
 ) {
     if (text == null) return
-
-    var finalText = text
-    if (formatBreakLine) finalText = finalText.replace("\n", "<br/>")
-
-    view.text = HtmlCompat.fromHtml(
-        finalText, HtmlCompat.FROM_HTML_SEPARATOR_LINE_BREAK_PARAGRAPH
+    val spannable = HtmlCompat.fromHtml(
+        text, HtmlCompat.FROM_HTML_SEPARATOR_LINE_BREAK_PARAGRAPH
                 or HtmlCompat.FROM_HTML_SEPARATOR_LINE_BREAK_HEADING
                 or HtmlCompat.FROM_HTML_SEPARATOR_LINE_BREAK_LIST_ITEM
                 or HtmlCompat.FROM_HTML_SEPARATOR_LINE_BREAK_LIST
                 or HtmlCompat.FROM_HTML_SEPARATOR_LINE_BREAK_DIV
                 or HtmlCompat.FROM_HTML_SEPARATOR_LINE_BREAK_BLOCKQUOTE
     )
+    view.text = spannable
 }
