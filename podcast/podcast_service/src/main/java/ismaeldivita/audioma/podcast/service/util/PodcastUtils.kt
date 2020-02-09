d2cc4033e34daade66cb0a3d16c53970ac54ac88
ismaeldivita <ismaeldivita@gmail.com>
2020-02-09 13:45:48 +0000

    Sanitize podcast title

--- podcast/podcast_service/src/main/java/ismaeldivita/audioma/podcast/service/util/PodcastUtils.kt
+++ podcast/podcast_service/src/main/java/ismaeldivita/audioma/podcast/service/util/PodcastUtils.kt
@@ -4,10 +4,12 @@ import org.apache.commons.text.StringEscapeUtils
 
 object PodcastUtils {
 
+    fun sanitizeTitle(text: String): String = StringEscapeUtils.unescapeHtml4(text)
+
     fun sanitizeDescription(text: String): String = text
-        .lines()
-        .joinToString(separator = "<br/>") { it.trim() }
-        .let(StringEscapeUtils::unescapeHtml4)
-        .replace("<![CDATA[", "")
-        .replace("]]>", "")
+            .lines()
+            .joinToString(separator = "<br/>") { it.trim() }
+            .let(StringEscapeUtils::unescapeHtml4)
+            .replace("<![CDATA[", "")
+            .replace("]]>", "")
 }
