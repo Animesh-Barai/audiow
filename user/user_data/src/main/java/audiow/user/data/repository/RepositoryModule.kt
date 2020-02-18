90e8e0f3a9aef404ec1bd2da7d3cfd39e081b91e
ismaeldivita <ismaeldivita@gmail.com>
2020-02-18 21:59:18 +0000

    Add UserSignOutCallback

--- user/user_data/src/main/java/audiow/user/data/repository/RepositoryModule.kt
+++ user/user_data/src/main/java/audiow/user/data/repository/RepositoryModule.kt
@@ -1,10 +1,12 @@
 package audiow.user.data.repository
 
+import audiow.core.common.SignOutCallback
 import audiow.core.data.repository.Repository
 import audiow.user.data.model.User
 import dagger.Binds
 import dagger.Module
 import dagger.Reusable
+import dagger.multibindings.IntoSet
 
 @Module
 interface RepositoryModule {
@@ -12,5 +14,9 @@ interface RepositoryModule {
     @Binds
     @Reusable
     fun bindUserRepository(repository: UserRepository): Repository<User>
+
+    @Binds
+    @IntoSet
+    fun bindSignOutCallback(callback: UserSignOutCallback): SignOutCallback
 
 }
---
+++ user/user_data/src/main/java/audiow/user/data/repository/UserSignOutCallback.kt
@@ -0,0 +1,14 @@
+package audiow.user.data.repository
+
+import audiow.core.common.SignOutCallback
+import audiow.core.data.repository.Repository
+import audiow.user.data.model.User
+import javax.inject.Inject
+
+class UserSignOutCallback @Inject constructor(
+    private val userRepository: Repository<User>
+) : SignOutCallback {
+
+    override fun onSignOut() = userRepository.clear()
+
+}
