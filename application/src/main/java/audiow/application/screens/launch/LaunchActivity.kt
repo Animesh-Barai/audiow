74b67aa5432b3ddf43d805b470e293fe9055bd40
ismaeldivita <ismaeldivita@gmail.com>
2020-02-20 01:06:59 +0000

    Handle sign-in result on LaunchScreen

--- application/src/main/java/audiow/application/screens/launch/LaunchActivity.kt
+++ application/src/main/java/audiow/application/screens/launch/LaunchActivity.kt
@@ -25,20 +25,36 @@ class LaunchActivity : DaggerAppCompatActivity() {
         viewModel = ViewModelProviders.of(this, vmFactory).get()
         viewModel.onCreate()
 
+        // TODO move to databinding
         observe(viewModel.state) {
             when (it) {
-                LaunchState.Initialized.Home -> {
-                    finish()
-                    startActivity(Intent(this, HomeActivity::class.java))
+                LaunchState.Initialized.Home -> launchHome()
+
+                LaunchState.Initialized.SignIn ->
+                    startActivityForResult(SignInActivity.getIntent(this), RC_SIGN_IN)
+
+                LaunchState.Loading,
+                LaunchState.Error -> {
                 }
-                LaunchState.Initialized.SignIn -> {
-                    finish()
-                    startActivity(Intent(this, SignInActivity::class.java))
-                }
-                LaunchState.Loading -> { }
-                LaunchState.Error -> { }
             }.exhaustive
         }
     }
 
+    private fun launchHome() {
+        finish()
+        startActivity(Intent(this, HomeActivity::class.java))
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when (requestCode to resultCode) {
+            RC_SIGN_IN to RESULT_OK -> launchHome()
+            RC_SIGN_IN to RESULT_CANCELED -> finish()
+        }
+    }
+
+    companion object {
+        private const val RC_SIGN_IN = 1000
+    }
 }
