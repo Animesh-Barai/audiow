f3de889e71f39df9b0f214f8e14642a705449341
ismaeldivita <ismaeldivita@gmail.com>
2020-02-22 15:59:16 +0000

    Add Module to UserDataModule

--- user/user_data/src/main/java/audiow/user/data/UserDataModule.kt
+++ user/user_data/src/main/java/audiow/user/data/UserDataModule.kt
@@ -3,13 +3,15 @@ package audiow.user.data
 import audiow.user.data.firebase.FirebaseModule
 import audiow.user.data.interactor.InteractorModule
 import audiow.user.data.repository.RepositoryModule
+import audiow.user.data.storage.DatabaseModule
 import dagger.Module
 
 @Module(
     includes = [
         RepositoryModule::class,
         InteractorModule::class,
-        FirebaseModule::class
+        FirebaseModule::class,
+        DatabaseModule::class
     ]
 )
 abstract class UserDataModule
