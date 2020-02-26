ba5219def819326ba15797df0588b5a337fd1023
ismaeldivita <ismaeldivita@gmail.com>
2020-02-26 22:54:41 +0000

    Add sha1 to String utils

--- core/src/main/java/audiow/core/util/standart/String.kt
+++ core/src/main/java/audiow/core/util/standart/String.kt
@@ -1,12 +1,29 @@
 package audiow.core.util.standart
 
+import java.security.MessageDigest
+
 fun String.replaceLastOccurrence(target: String, replacement: String): String {
     val lastIndexOfTarget = lastIndexOf(target)
     return if (lastIndexOfTarget == -1) {
         this
     } else {
         StringBuilder(this)
-                .replace(lastIndexOfTarget, lastIndexOfTarget + target.length, replacement)
-                .toString()
+            .replace(lastIndexOfTarget, lastIndexOfTarget + target.length, replacement)
+            .toString()
     }
+}
+
+fun String.sha1(): String {
+    val hexChars = "0123456789ABCDEF"
+    val bytes = MessageDigest
+        .getInstance("SHA-1")
+        .digest(toByteArray())
+    val result = StringBuilder(bytes.size * 2)
+
+    bytes.forEach {
+        val i = it.toInt()
+        result.append(hexChars[i shr 4 and 0x0f])
+        result.append(hexChars[i and 0x0f])
+    }
+    return result.toString()
 }
