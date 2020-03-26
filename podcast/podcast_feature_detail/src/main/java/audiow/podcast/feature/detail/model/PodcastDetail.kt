d1ae33a376ea9d4bc42a5e372fed2efad010476d
ismaeldivita <ismaeldivita@gmail.com>
2020-03-26 19:24:11 +0000

    Remove unused PodcastDetail

--- podcast/podcast_feature_detail/src/main/java/audiow/podcast/feature/detail/model/PodcastDetail.kt
+++
@@ -1,8 +0,0 @@
-package audiow.podcast.feature.detail.model
-
-import audiow.podcast.data.model.Podcast
-
-internal data class PodcastDetail(
-    val podcast: Podcast,
-    val isSubscribed: Boolean
-)
